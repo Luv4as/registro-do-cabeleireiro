64d4737af51e06c64af88f032c5cf955a2b1fbef
Lucas Venancio <lucas.venancio@outlook.com>
2026-04-15 01:46:51 -0300

    fix: reestruturação de cadastro

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,15 @@ import type { NextConfig } from "next";
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default withFlowbiteReact(nextConfig);
